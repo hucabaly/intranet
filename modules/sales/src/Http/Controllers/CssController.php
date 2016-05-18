eb74e5350548d53f16a4151fe2331b80aacf271e
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-18 18:31:54 +0700

    them cac function tao CSS

--- modules/sales/src/Http/Controllers/CssController.php
+++ modules/sales/src/Http/Controllers/CssController.php
@@ -3,6 +3,7 @@
 namespace Rikkei\Sales\Http\Controllers;
 use Rikkei\Core\Http\Controllers\Controller as Controller;
 use Auth;
+use DB;
 use Rikkei\Core\Model\User;
 use Illuminate\Http\Request;
 use Rikkei\Sales\Model\ProjectType;
@@ -12,24 +13,82 @@ use Rikkei\Sales\Model\Css;
 class CssController extends Controller
 {
 
+
+    public function create()
+    {
+        $user = Auth::user();
+        $projects = ProjectType::all();
+        $teams = Teams::all();
+        
+        return view(
+                    'sales::create_css', 
+                    [
+                        'user'      => $user, 
+                        "projects"  => $projects,
+                        "teams"     => $teams
+                    ]
+                );
+    }
+
+    public function createcss(){
+        if(Auth::check() && $_POST){
+            //echo "<pre>"; var_dump($_POST);die;
+            $start_date = date('Y-m-d',strtotime($_POST["start_date"]));
+            $end_date   = date('Y-m-d',strtotime($_POST["end_date"]));
+
+            //insert vao bang css
+            $css = new Css;
+            $css->user_id           = $_POST["user_id"];
+            $css->company_name      = $_POST["company_name"];
+            $css->customer_name     = $_POST["customer_name"];
+            $css->project_name      = $_POST["project_name"];
+            $css->brse_name         = $_POST["brse_name"];
+            $css->start_date        = $start_date;
+            $css->end_date          = $end_date;
+            $css->pm_name           = $_POST["pm_name"];
+            $css->project_type_id   = $_POST["project_type_id"];
+            $css->token             = md5(rand());
+            $css->save();
+
+            //Neu user chua co ten Tieng Nhat thi update
+            $user = Auth::user();
+            if(!$user->japanese_name){
+                $user->japanese_name = $_POST["japanese_name"];
+                $user->save();
+            }
+            
+            //insert vao bang css_team
+            $teams = $_POST["teams"];
+            foreach($teams as $k => $v){
+                DB::table('css_team')->insert(
+                     array(
+                        'css_id'    =>   $css->id, 
+                        'team_id'   =>   $k
+                     )
+                );
+            }
+
+            echo "success";
+        }
+    }
+
     public function make($token,$id){
         $css = Css::where('id', $id)
                ->where('token', $token)
                ->first();
+        
         if($css){
             $user = User::find($css->user_id);
-
             return view(
                     'sales::makecss', 
                     [
-                        'css'   => $css, 
-                        "user"  => $user
+                       'css'   => $css, 
+                       "user"  => $user
                     ]
+  
                 );
-        }
-        else{
+        }else{
             return redirect("/");
         }
-        
     }
 }
