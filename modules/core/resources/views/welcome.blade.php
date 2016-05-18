eddcd0a3d278d4ae825b0d6ffefbe7a033e5c285
giangsoda <giangsoda@gmail.com>
2016-05-18 13:01:05 +0700

    fix error login by accout rikkei

--- modules/core/resources/views/welcome.blade.php
+++ modules/core/resources/views/welcome.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>RikkeiSoft's Intranet</title>
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div class="jumbotron">
-            <div class="container">
-                <h1>RikkeiSoft's Intranet</h1>
-                <p>...</p>
-                <p><a class="btn btn-primary btn-lg" href="{{ url('auth/connect', ['google']) }}" role="button">Sign in</a></p>
-            </div>
-        </div>
-    </body>
-</html>
+@extends('layouts.guest')
+
+@section('content')
+<h1>RikkeiSoft's Intranet</h1>
+<p>...</p>
+<p>
+    <a class="btn btn-primary btn-lg" href="{{ url('auth/connect', ['google']) }}" role="button">Sign in</a>
+</p>
+@endsection
