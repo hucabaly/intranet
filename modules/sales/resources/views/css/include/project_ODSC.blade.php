a70ac11be66160c1a2691ccbb2ba22380015884d
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-20 11:52:09 +0700

    2 file view tương ứng của 2 loại project

---
+++ modules/sales/resources/views/css/include/project_ODSC.blade.php
@@ -0,0 +1,300 @@
+<table class="table table-bordered bang1">
+      <tr><td colspan="5" class="top">プロジェクト情報</td></tr>
+      <tr>
+        <td class="title">ラボ契約名</td>
+        <td><input type="text" id="pj_name" name="pj_name" readonly=""></td>
+        <td class="title2">ラボ期間</td>
+        <td><input type="text" id="pj_time" name="pj_time" readonly=""></td>
+        <td class="tongdiem">ご記入日：</td>
+    </tr>
+    <tr>
+        <td class="title">Rikkeisoft側営業者の氏名</td>
+        <td><input type="text" id="rikkei_name" name="rikkei_name" readonly=""></td>
+        <td class="title2">お客様側代表者ご氏名</td>
+        <td><input type="text" id="cus_name" name="cus_name" readonly=""></td>
+        <td rowspan="3" class="diemso-osdc">
+            <div>合計点数：</div>
+            <div class="diem">68.00</div>
+        </td>
+    </tr>
+    <tr>
+        <td class="title">プロジェクト担当者の氏名</td>
+        <td><input type="text" id="pm_name" name="pm_name" readonly=""></td>
+        <td class="title2">評価者ご氏名</td>
+        <td><input type="text" id="mk_name" name="mk_name" readonly=""></td>
+
+    </tr>
+    <tr>
+        <td class="title">プロジェクトのBrSE担当者の氏名</td>
+        <td><input type="text" id="brse_name" name="brse_name" readonly=""></td>
+        <td class="title2">評価者のメールアドレス</td>
+        <td><input type="text" id="mk_email" name="mk_email" readonly=""></td>
+
+    </tr>
+</table>
+
+<table class="table table-bordered bang2 table-osdc">
+  <!-- header -->
+  <tr class="header">
+    <td>質問</td>
+    <td>内容詳細</td>
+    <td class="reply">回答</td>
+    <td class="comment">コメント</td>
+</tr>
+
+<!-- muc to 1 -->
+<tr class="mucto">
+    <td class="title" colspan="3">I.能力及び成績品質</td>
+
+    <td class="title2">（★）と（★★）の項目がある場合はコメントをご記入ください。</td>
+
+</tr>
+
+<!-- muc be 1 -->
+<tr class="mucbe">
+    <td class="title" colspan="3">1.能力/スキル</td>
+
+    <td class="title2"></td>
+    </tr>
+
+    <!-- cau 1 -->
+    <tr class="cau">
+        <td class="title" colspan="2">①　弊社スタッフの能力は割り当てられた作業に適していましたか。</td>
+
+        <td class="title2">★★ - やや不満</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 2 -->
+    <tr class="cau">
+        <td class="title" colspan="2">②　弊社スタッフのテクニカルスキルについてどう思われましたか。（要求の理解力、設計、コーディング能力等）</td>
+
+        <td class="title2">★★★ - 普通</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 3 -->
+    <tr class="cau">
+        <td class="title" colspan="2">③　弊社スタッフの問題解決能力をどう思われましたか。</td>
+
+        <td class="title2">★★★★★ - 満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 4 -->
+    <tr class="cau">
+        <td class="title" colspan="2">④　弊社スタッフのチームワークスキルについてどう思われますか。</td>
+
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 5 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑤　プロジェクトチームのスケジュールの遵守度はいかがでしたか。（進捗状況がスケジュール通りであったかどうか、納品は納期通りであったかどうか等） </td>
+
+        <td class="title2">★★ - やや不満</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 6 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑥　プロジェクトチームの報告書の作成能力とその質はいかがでしたか。</td>
+
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 7 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑦　関連成果物に要求変更が十分に反映されていましたか。（要求仕様書、ソースコード、テストドキュメント等）</td>
+
+        <td class="title2">★★★★★ - 満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- muc be 2 -->  
+    <tr class="mucbe">
+        <td class="title" colspan="3">2.作業の成果</td>
+
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 8 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑧　弊社スタッフの作業完了度について評価して下さい。</td>
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 9 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑨　弊社スタッフの作業の質についてどう思われますか。</td>
+        <td class="title2">★★★ - 普通</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 10 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑩　弊社スタッフの仕事対応のレスポンスについてどう思われますか。</td>
+        <td class="title2">★★ - やや不満</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- muc to 2 -->
+    <tr class="mucto">
+        <td class="title" colspan="3">II.サービスの品質</td>
+
+        <td class="title2"></td>
+
+    </tr>
+
+    <!-- muc be 3 -->
+    <tr class="mucbe">
+        <td class="title" colspan="3">3.責任感</td>
+
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 11 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑪　弊社スタッフの作業に対する責任感・義務感について評価して下さい。 </td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 12 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑫　担当したプロジェクト、弊社、貴社の利益に対する弊社スタッフの責任意識についてどう感じられましたか。 </td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 13 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑬　弊社スタッフの、仕事や私事的な障害を乗り越える能力についてどう思われましたか。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- muc be 4 -->
+    <tr class="mucbe">
+        <td class="title" colspan="3">4.労働規律</td>
+
+        <td class="title2"></td>
+    </tr>
+
+    
+    <!-- cau 14 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑭　弊社スタッフは就業時間を有効に利用しましたか。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 15 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑮　弊社スタッフの貴社の一般的な規律に対する遵守度はどうでしたか。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- muc to 3 -->
+    <tr class="mucto">
+        <td class="title" colspan="3">III.日本語でのコミュニケーション能力についての評価</td>
+
+        <td class="title2"></td>
+
+    </tr>
+
+    <tr >
+        <td class="title" colspan="3">担当BrSEの氏名：</td>
+
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 16 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑯　メールにおける日本語翻訳の質について評価してください。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 17 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑰　各種ドキュメントにおける日本語翻訳の質について評価してください。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+    
+    <!-- cau 18 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑱　会議（電話会議、TV会議、チャット等を含む）における通訳の日本語の質について評価してください。</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- muc to 4 -->
+    <tr class="mucto">
+        <td class="title" colspan="4">IV.OSDCの担当者についての評価</td>
+    </tr>
+
+    <tr >
+        <td class="title" colspan="3">担当者の氏名：</td>
+
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 19 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑲　ラボ担当者の責任感について評価してください。（各プロジェクトの確認、リソース調整能力など）</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- cau 20 -->
+    <tr class="cau">
+        <td class="title" colspan="2">⑳　貴社とプロジェクトチーム（又は弊社）との関係はいかがでしたでしょうか。仕事上やそれ以外も含む直接の会話やメールを通してのやりとりによる関係</td>
+        
+        <td class="title2">★★★★ - やや満足</td>
+        <td class="title2"></td>
+    </tr>
+    
+
+    <!-- muc to 5 -->
+    <tr class="mucto">
+        <td class="title" colspan="4">V. 全体</td>
+    </tr>
+
+    <!-- cau tong quat -->
+    <tr class="cau">
+        <td class="title" colspan="2">全全体的に、弊社の提供した製品とサービスにご満足いただけましたか。</td>
+        
+        <td class="title2">★★★★★ - 満足</td>
+        <td class="title2"></td>
+    </tr>
+
+    <!-- danh gia chung -->
+    <tr class="cau">
+        <td class="title" >
+            <div>このプロジェクトで貴社にさらに満足いただくには、</div>
+            <div>・プロジェクトの品質</div>
+            <div>・プロジェクト管理能力</div>
+            <div>・日本語でのコミュニケーション能力</div>
+            <div>を向上するために、弊社は何をすべきであったと思われますか。</div>
+            <div>（最も重要と思われる項目を3つ記入してください。）</div>
+        </td>
+        
+        <td class="title2" colspan="2"></td>
+        <td class="title2"></td>
+    </tr>
+</table>
