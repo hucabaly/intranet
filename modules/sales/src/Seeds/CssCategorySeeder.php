769a86aebaa73e66c6cc457ac7e8ef51c89b23e8
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-26 18:38:22 +0700

    insert du lieu cho 2 bang css_category va css_question

--- modules/sales/src/Seeds/CssCategorySeeder.php
+++ modules/sales/src/Seeds/CssCategorySeeder.php
@@ -23,67 +23,67 @@ class CssCategorySeeder extends Seeder
                 'parent_id' => '0'
             ],
             [
-                'name' => 'Chất lượng dự án',
+                'name' => 'I. Chất lượng dự án',
                 'parent_id' => '2'
             ],
             [
-                'name' => 'Về quản lý dự án',
+                'name' => 'II. Về quản lý dự án',
                 'parent_id' => '2'
             ],
             [
-                'name' => 'Về đánh giá BrSE',
+                'name' => 'III. Về đánh giá BrSE',
                 'parent_id' => '2'
             ],
             [
-                'name' => 'Về chất lượng giai đoạn phân tích yêu cầu',
+                'name' => '1. Về chất lượng giai đoạn phân tích yêu cầu',
                 'parent_id' => '3'
             ],
             [
-                'name' => 'Về chất lượng giai đoạn thiết kế',
+                'name' => '2. Về chất lượng giai đoạn thiết kế',
                 'parent_id' => '3'
             ],
             [
-                'name' => 'Về chất lượng giai đoạn coding',
+                'name' => '3. Về chất lượng giai đoạn coding',
                 'parent_id' => '3'
             ],
             [
-                'name' => 'Về chất lượng giai đoạn test',
+                'name' => '4. Về chất lượng giai đoạn test',
                 'parent_id' => '3'
             ],
             [
-                'name' => 'Về Chất lượng support khách hàng',
+                'name' => '5. Về Chất lượng support khách hàng',
                 'parent_id' => '3'
             ],
             [
-                'name' => 'Về năng lực và thành tích',
+                'name' => 'I. Về năng lực và thành tích',
                 'parent_id' => '1'
             ],
             [
-                'name' => 'Chất lượng dịch vụ',
+                'name' => 'II. Chất lượng dịch vụ',
                 'parent_id' => '1'
             ],
             [
-                'name' => 'Khả năng communication bằng tiếng Nhật',
+                'name' => 'III. Khả năng communication bằng tiếng Nhật',
                 'parent_id' => '1'
             ],
             [
-                'name' => 'Đánh giá người phụ trách OSDC',
+                'name' => 'IV. Đánh giá người phụ trách OSDC',
                 'parent_id' => '1'
             ],
             [
-                'name' => 'Kỹ năng, năng lực',
+                'name' => '1. Kỹ năng, năng lực',
                 'parent_id' => '11'
             ],
             [
-                'name' => 'Hiệu quả làm việc',
+                'name' => '2. Hiệu quả làm việc',
                 'parent_id' => '11'
             ],
             [
-                'name' => 'Tính trách nhiệm',
+                'name' => '3. Tinh trách nhiệm',
                 'parent_id' => '12'
             ],
             [
-                'name' => 'Nội quy lao động',
+                'name' => '4. Nội quy lao động',
                 'parent_id' => '12'
             ]
             
--- modules/sales/src/Seeds/CssQuestionSeeder.php
+++ modules/sales/src/Seeds/CssQuestionSeeder.php
@@ -15,84 +15,169 @@ class CssQuestionSeeder extends Seeder
     {
         $dataDemo = [
             [
-                'content' => 'Khả năng phân tích yêu cầu và lý giải yêu cầu của team đánh giá thế nào',
+                'content' => '1. Khả năng phân tích yêu cầu và lý giải yêu cầu của team đánh giá thế nào',
                 'category_id' => '6'
             ],
             [
-                'content' => 'Các thay đổi yêu cầu có được đáp ứng đầy đủ vào sản phẩm không (tài liệu spec, source code, …)',
+                'content' => '2. Các thay đổi yêu cầu có được đáp ứng đầy đủ vào sản phẩm không (tài liệu spec, source code, …)',
                 'category_id' => '6'
             ],
             [
-                'content' => 'Tài liệu thiết kế của team có nội dung chính xác, rõ ràng không?',
+                'content' => '3. Tài liệu thiết kế của team có nội dung chính xác, rõ ràng không?',
                 'category_id' => '7'
             ],
             [
-                'content' => 'Solution của thiết kế có thích hợp không?',
+                'content' => '4. Solution của thiết kế có thích hợp không?',
                 'category_id' => '7'
             ],
             [
-                'content' => 'Source code có được viết rõ ràng không? (có đúng convention không, có comment đầy đủ không)',
+                'content' => '5. Source code có được viết rõ ràng không? (có đúng convention không, có comment đầy đủ không)',
                 'category_id' => '8'
             ],
             [
-                'content' => 'Source code có phản ánh đầy đủ nội dung spec không?',
+                'content' => '6. Source code có phản ánh đầy đủ nội dung spec không?',
                 'category_id' => '8'
             ],
             [
-                'content' => 'Bạn nghĩ thế nào về khả năng làm unit test của team',
+                'content' => '7. Bạn nghĩ thế nào về khả năng làm unit test của team',
                 'category_id' => '8'
             ],
             [
-                'content' => 'Chất lượng của tài liệu test do team tạo ra thế nào?',
+                'content' => '8. Chất lượng của tài liệu test do team tạo ra thế nào?',
                 'category_id' => '9'
             ],
             [
-                'content' => 'Chất lượng thực hiện test của team thế nào?',
+                'content' => '9. Chất lượng thực hiện test của team thế nào?',
                 'category_id' => '9'
             ],
             [
-                'content' => 'Chất lượng đối ứng bug của team thế nào?',
+                'content' => '10. Chất lượng đối ứng bug của team thế nào?',
                 'category_id' => '9'
             ],
             [
-                'content' => 'Response của team có nhanh hay không?',
+                'content' => '11. Response của team có nhanh hay không?',
                 'category_id' => '10'
             ],
             [
-                'content' => 'Solution mà team đề xuất có thích hợp hay không?',
+                'content' => '12. Solution mà team đề xuất có thích hợp hay không?',
                 'category_id' => '10'
             ],
             [
-                'content' => 'Team có đảm bảo được schedule không? (Thực hiện đúng tiến độ như schedule đề ra, release đúng thời gian kế hoạch)',
+                'content' => '13. Team có đảm bảo được schedule không? (Thực hiện đúng tiến độ như schedule đề ra, release đúng thời gian kế hoạch)',
                 'category_id' => '4'
             ],
             [
-                'content' => 'Khả năng tạo tài liệu báo cáo của team thế nào?',
+                'content' => '14. Khả năng tạo tài liệu báo cáo của team thế nào?',
                 'category_id' => '4'
             ],
             [
-                'content' => 'Khả năng quản lý rủi ro, quản lý vấn đề của team thế nào?',
+                'content' => '15. Khả năng quản lý rủi ro, quản lý vấn đề của team thế nào?',
                 'category_id' => '4'
             ],
             [
-                'content' => 'Team làm việc có nhiệt tình, hăng hái hay không?',
+                'content' => '16. Team làm việc có nhiệt tình, hăng hái hay không?',
                 'category_id' => '4'
             ],
             [
-                'content' => 'Quan hệ và thái độ của nhân viên trong team khi làm việc với khách hàng có tốt hay không?',
+                'content' => '17. Quan hệ và thái độ của nhân viên trong team khi làm việc với khách hàng có tốt hay không?',
                 'category_id' => '4'
             ],
             [
-                'content' => 'Khả năng kỹ thuật của BrSE đánh giá thế nào? (khả năng phân tích và truyền đạt yêu cầu, thiết kế, coding,…)',
+                'content' => '18. Khả năng kỹ thuật của BrSE đánh giá thế nào? (khả năng phân tích và truyền đạt yêu cầu, thiết kế, coding,…)',
                 'category_id' => '5'
             ],
             [
-                'content' => 'Khả năng tiếng Nhật của BrSE thế nào (mail tiếng Nhật, chat tiếng Nhật, nói chuyện tiếng Nhật, dịch tài liệu tiếng Nhật,…)',
+                'content' => '19. Khả năng tiếng Nhật của BrSE thế nào (mail tiếng Nhật, chat tiếng Nhật, nói chuyện tiếng Nhật, dịch tài liệu tiếng Nhật,…)',
                 'category_id' => '5'
             ],
             [
-                'content' => 'Khả năng giải quyết vấn đề và support cho dự án đánh giá thế nào',
+                'content' => '20. Khả năng giải quyết vấn đề và support cho dự án đánh giá thế nào',
                 'category_id' => '5'
+            ],
+            
+            
+            
+            
+            
+            [
+                'content' => '1. Năng lực của nhân viên công ty cung cấp cho các bạn phù hợp chứ?',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '2. Kỹ năng về kỹ thuật của nhân viên công ty cung cấp cho các bạn phù hợp chứ (khả năng phân tích yêu cầu, thiết kế, coding,..)',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '3. Khả năng giải quyết vấn đề của nhân viên công ty cung cấp cho các bạn thế nào?',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '4. Kỹ năng làm việc nhóm của các thành viên trong team thế nào?',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '5. Các project trong OSDC có đảm bảo tiến độ đã nếu trong schedule hay không?',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '6. Khả năng tạo tài liệu báo cáo, report hàng ngày của member trong OSDC có tốt không?',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '7. Các thay đổi về requirement có được phản ánh đầy đủ vào sản phẩm hay không? (tài liệu requiremetn, source code,…)',
+                'category_id' => '15'
+            ],
+            [
+                'content' => '8. Các task của member có được hoàn thành 1 cách triệt để hay không?',
+                'category_id' => '16'
+            ],
+            [
+                'content' => '9. Việc hoàn thành task của member chất lượng thế nào?',
+                'category_id' => '16'
+            ],
+            [
+                'content' => '10. Thời gian response của member có tốt không?',
+                'category_id' => '16'
+            ],
+            [
+                'content' => '11. Trách nhiệm và thái độ làm việc của member trong team thế nào?',
+                'category_id' => '17'
+            ],
+            [
+                'content' => '12. Member có trách nhiệm và ý thức được rằng phải nỗ lực vì lợi ích của công ty và của khách hàng hay không?',
+                'category_id' => '17'
+            ],
+            [
+                'content' => '13. Member trong team có cố gắng khắc vụ những sự cố hay việc cá nhân để hoàn thành dự án tốt hay không?',
+                'category_id' => '17'
+            ],
+            [
+                'content' => '14. Member có dùng hiệu quả thời gian làm việc của mình hay không? (làm việc riêng trong giờ làm việc, làm các công việc khác,…)',
+                'category_id' => '18'
+            ],
+            [
+                'content' => '15. Member có đảm bảo tuân thủ đúng các quy tắc nội dung đã thống nhất giữa 2 công ty hay không?',
+                'category_id' => '18'
+            ],
+            [
+                'content' => '16. Khả năng đọc hiểu và dịch mail tiếng Nhật có tốt không?',
+                'category_id' => '13'
+            ],
+            [
+                'content' => '17. Khả năng dịch các tài liệu tiếng Nhật có tốt không?',
+                'category_id' => '13'
+            ],
+            [
+                'content' => '18. Chất lượng dịch tiếng Nhật trong các buổi họp với khách hàng có tốt hay không?',
+                'category_id' => '13'
+            ],
+            [
+                'content' => '19. Hãy đánh giá trách nhiệm của người phụ trách OSDC (quan tâm đến tình hình các dự án, khả năng điều chỉnh nguồn lực,…)',
+                'category_id' => '14'
+            ],
+            [
+                'content' => '20. Quan hệ giữa Team và khách hàng có tốt hay không?',
+                'category_id' => '14'
             ]
         ];
         foreach ($dataDemo as $data) {
