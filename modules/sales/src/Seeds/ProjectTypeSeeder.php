e618871f9c4cff3bfa6e23aab35b96b720864e4b
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-30 15:43:26 +0700

    file seed table project_type

---
+++ modules/sales/src/Seeds/ProjectTypeSeeder.php
@@ -0,0 +1,30 @@
+<?php
+namespace Rikkei\Sales\Seeds;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class ProjectTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataDemo = [
+            [
+                'name' => 'OSDC',
+            ],
+            [
+                'name' => 'Project base',
+            ]
+        ];
+        foreach ($dataDemo as $data) {
+            if (! DB::table('project_type')->select('id')->where('name', $data['name'])->get()) {
+                DB::table('project_type')->insert($data);
+            }
+        }
+    }
+}
