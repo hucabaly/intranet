bd38731ea0da5ec5596e3d20838bf1aeb8707b20
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-30 12:04:35 +0700

    trang huy khong lam bai danh gia

---
+++ modules/sales/resources/views/css/cancel.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="row">
+    <div class="col-xs-12">
+        <div class="col-md-12">
+          <div class="box box-solid">
+            <div class="box-body">
+              <div class="box-group" id="accordion">
+                <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+                <div class="panel box box-primary">
+                  <div class="box-header with-border">
+                    <h4 class="box-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                        {{trans("sales::view.Cancel make title")}}
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="box-body">
+                      {{trans("sales::view.Cancel make")}}
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        
+    </div>
+</div>    
+@endsection
