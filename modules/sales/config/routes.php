9551f1ea3ae79e82a0614d3e9968159194db3401
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-31 09:16:42 +0700

    fix bug guest khong co quyen truy cap vao function insert css vao database

--- modules/sales/config/routes.php
+++ modules/sales/config/routes.php
@@ -7,13 +7,12 @@ Route::group([
 	Route::get('css/create', 'CssController@create')->name('create');
 	Route::get('css/update/{id}', 'CssController@update')->name('update');
 	Route::post('css/save', 'CssController@save')->name('save');
-        Route::post('css/saveResult', 'CssController@saveResult')->name('saveResult');
-	Route::get('css/preview/{token}/{id}', 'CssController@preview')->name('saveResult');
+        Route::get('css/preview/{token}/{id}', 'CssController@preview')->name('saveResult');
         Route::get('sales/css/list', 'CssController@grid')->name('list');
         Route::get('css/view/{id}', 'CssController@view')->name('view');
         Route::get('css/detail/{id}', 'CssController@detail')->name('detail');
-        
         Route::get('css/cancel', 'CssController@cancelMake')->name('cancel');
 });
 Route::get('css/success/{id}', 'CssController@success')->name('success');
 Route::get('css/make/{token}/{id}', 'CssController@make')->name('make') ;
+Route::post('css/saveResult', 'CssController@saveResult')->name('saveResult');
