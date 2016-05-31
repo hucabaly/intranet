11cf8ab050998c4dd9afa006deecd88097307ab3
Nguyen Thanh Hung <hungnt2@rikkeisoft.com>
2016-05-31 17:42:50 +0700

    fix language

--- modules/sales/resources/lang/vi/message.php
+++ modules/sales/resources/lang/vi/message.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'Name validate required' => 'Bạn chưa điền họ và tên',
-    'Email validate required' => 'Bạn chưa điền email',
+    'Name validate required' => 'Quý khách chưa điền họ và tên',
+    'Email validate required' => 'Quý khách chưa điền email',
     'Email validate address' => 'Địa chỉ email của bạn không đúng định dạng',
-    'Total mark validate required' => 'Bạn chưa chọn điểm tổng quát',
-    'Question comment required' => "Bạn hãy comment các đánh giá là 1 hoặc 2 *",
+    'Total mark validate required' => 'Quý khách chưa chọn điểm tổng quát',
+    'Question comment required' => "Quý khách hãy comment các đánh giá là 1 hoặc 2 *",
     'Mark confirm' => "Số điểm hiện tại của CSS là :mark Điểm. Bạn có chắc muốn hoàn thành CSS tại đây không?",
-    'Make confirm' => "Bạn đang đăng nhập với tư cách là nhân viên Rikkeisoft để làm phiếu điều tra này, bạn có chắc là muốn thực hiện phiếu điều tra này hay không?", 
+    'Make confirm' => "Quý khách đang đăng nhập với tư cách là nhân viên Rikkeisoft để làm phiếu điều tra này, bạn có chắc là muốn thực hiện phiếu điều tra này hay không?", 
+    'Proposed required' => 'Xin quý khách góp ý để chúng tôi có thể cải thiện chất lượng dự án tốt hơn',
 ];
