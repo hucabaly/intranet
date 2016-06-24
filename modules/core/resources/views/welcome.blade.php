8b021abdbedb8c83e9b0e58b4e03944d7e4df8fa
giangsoda <giangsoda@gmail.com>
2016-06-24 14:35:55 +0700

    #94: login page

--- modules/core/resources/views/welcome.blade.php
+++ modules/core/resources/views/welcome.blade.php
@@ -5,12 +5,27 @@ Login
 
 @section('content')
 <div class="login-wrapper">
-    <h1 class="login-title">{{ trans('core::view.Welcome to Rikkeisoft Intranet !!!') }}</h1><!-- /.login-logo -->
+    <h1 class="login-title">
+        <img src="{{ URL::asset('img/logo_login.png') }}" />
+    </h1><!-- /.login-logo -->
     <div class="login-action">
         <p>
-            <a class="btn btn-primary btn-lg login-button" href="{{ url('auth/connect', ['google']) }}" 
-               role="button">{{ trans('core::view.Login with Rikkeisoft Account') }}</a>
+            <a class="btn login-button" href="{{ url('auth/connect', ['google']) }}" 
+               role="button">
+                <img src="{{ URL::asset('img/login-button.png') }}" />
+            </a>
         </p>
     </div><!-- /.login-box-action -->
 </div><!-- /.login-wrapper -->
+@endsection
+
+@section('script')
+<script src="{{ URL::asset('adminlte/plugins/jQuery/jQuery-2.2.0.min.js') }}"></script>
+<script src="{{ URL::asset('js/jquery.backstretch.min.js') }}"></script>
+<script>
+    jQuery(document).ready(function($) {
+        $.backstretch('{{ URL::asset('img/login-background.png') }}');
+    });
+    
+</script>
 @endsection
